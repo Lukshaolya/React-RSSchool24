4d7947aba723de56765dd1610e4b29ffc7203a45
Olya Luksha <lukshaolya98@gmail.com>
2024-07-27 18:28:22 +0200

    feat: use useContest for themes

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,14 +1,32 @@
+import { useEffect, useState } from 'react';
 import { NavLink } from 'react-router-dom';
-import logo from '../../assets/logo.png';
+import { THEME_DARK, THEME_LIGHT, useTheme } from '../../context/ThemeProvider';
+import LOGO_DARK from '../../assets/logo.png';
+import LOGO_LIGHT from '../../assets/logo-light-2.png';
 import styles from './Header.module.css';
 
 const Header = () => {
+  const isTheme = useTheme();
+  const [icon, setIcon] = useState(LOGO_DARK);
+  useEffect(() => {
+    switch (isTheme.theme) {
+      case THEME_DARK:
+        setIcon(LOGO_DARK);
+        break;
+      case THEME_LIGHT:
+        setIcon(LOGO_LIGHT);
+        break;
+      default:
+        setIcon(LOGO_DARK);
+    }
+  }, [isTheme]);
+  console.log(isTheme);
   return (
     <div>
       <div className={styles.container}>
         <header>
           <div className={styles.header_logo}>
-            <img src={logo} alt="logo" />
+            <img src={icon} alt="logo" />
           </div>
           <ul className={styles.header_nav_block}>
             <li>
@@ -18,6 +36,20 @@ const Header = () => {
               <NavLink to="*">Not found page</NavLink>
             </li>
           </ul>
+          <div className={styles.change_theme_container}>
+            <button
+              className={
+                isTheme.theme === THEME_DARK
+                  ? styles.change_theme
+                  : styles.change_theme_light
+              }
+              onClick={() => {
+                isTheme.change();
+              }}
+            >
+              {isTheme.theme === THEME_DARK ? THEME_LIGHT : THEME_DARK}
+            </button>
+          </div>
         </header>
       </div>
     </div>
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ThemeProvider } from './context/ThemeProvider.tsx';
 
 import App from './App.tsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 );
